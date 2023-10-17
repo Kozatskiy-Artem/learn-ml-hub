4c70cdbb4085fa90acfa79f7fa2f2ca32d0f81b6
Artem Kozatskyi <artemfury@gmail.com>
2023-10-17 17:51:31 +0300

    Developed information pages with adaptive interface

--- info_pages/urls.py
+++ info_pages/urls.py
@@ -6,4 +6,8 @@ app_name = 'info_pages'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('ml/', views.ml_page, name='ml'),
+    path('ml/types/', views.ml_types_page, name='ml_types'),
+    path('neural_networks/', views.neural_networks_page, name='neural_networks'),
+    path('deep_learning/', views.deep_learning_page, name='deep_learning'),
 ]
--- info_pages/views.py
+++ info_pages/views.py
@@ -5,3 +5,27 @@ def home(request):
     """"""
 
     return render(request, 'info_pages/home.html')
+
+
+def ml_page(request):
+    """"""
+
+    return render(request, 'info_pages/ml.html')
+
+
+def ml_types_page(request):
+    """"""
+
+    return render(request, 'info_pages/ml_types.html')
+
+
+def neural_networks_page(request):
+    """"""
+
+    return render(request, 'info_pages/neural_networks.html')
+
+
+def deep_learning_page(request):
+    """"""
+
+    return render(request, 'info_pages/deep_learning.html')
